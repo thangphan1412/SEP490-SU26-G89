c33212c4160ca80bfe3d47a6a9bf70940b1ac187
PhanManhThang <thangphan14122003@gmail.com>
2026-05-26 13:35:34 +0700

    feat: setup java jx

---
+++ desktop-javafx/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.be.desktopjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.be.desktopjavafx to javafx.fxml;
+    exports org.be.desktopjavafx;
+}
