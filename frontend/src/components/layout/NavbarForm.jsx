ec1fe5a38bcb9ee1d2f7e55d29455db06c3ecc52
PhanManhThang <thangphan14122003@gmail.com>
2026-06-22 23:37:49 +0700

    feat: fix css header

--- frontend/src/components/layout/NavbarForm.jsx
+++ frontend/src/components/layout/NavbarForm.jsx
@@ -1,6 +1,10 @@
+import "../../assets/styles/css/layoutStyles/Navbar.css"
+
 function NavbarForm(){
     return(
-        <div className="navbar-form-container-fluid"></div>
+        <div className="navbar-form-container-fluid">
+
+        </div>
     )
 }
 export default NavbarForm
