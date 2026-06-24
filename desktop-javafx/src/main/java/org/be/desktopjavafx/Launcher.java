fec189ef84e0108c473941f2291dac361976aa91
PhanManhThang <thangphan14122003@gmail.com>
2026-06-24 19:02:41 +0700

    feat: deleted modul javajx

--- desktop-javafx/src/main/java/org/be/desktopjavafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.be.desktopjavafx;
-
-import javafx.application.Application;
-// clss main
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
