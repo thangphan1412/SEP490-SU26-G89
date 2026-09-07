ec0c1494d55ed48745549ca4ba83075670940c1f
PhanManhThang <thangphan14122003@gmail.com>
2026-09-07 13:13:30 +0700

    fix authen

--- frontend/src/config/api/axiosClient.js
+++ frontend/src/config/api/axiosClient.js
@@ -36,6 +36,19 @@ axiosClient.interceptors.request.use((config) => {
     }
 
     return config;
-});
 
+});
+axiosClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token");
+
+            if (window.location.pathname !== "/login") {
+                window.location.href = "/login";
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 export default axiosClient;
